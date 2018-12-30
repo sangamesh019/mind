54af1d0367940252074df9097d42e9886f49375c
sangamesh019 <sangamhiremath@live.com>
2018-12-30 17:02:35 +0530

    updated 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './component/header/header.component';
+import { SectionComponent } from './component/section/section.component';
+import { TeamComponent } from './component/team/team.component';
+import { FooterComponent } from './component/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    SectionComponent,
+    TeamComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
